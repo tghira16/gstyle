9413cdf5cc6b2bc592b71e3e2fef025b18e6be07
Giriraj Hiranandani <tghira16@gmail.com>
2022-06-21 16:18:38 +1000

    minor update

--- pubplot.m
+++ pubplot.m
@@ -3,12 +3,12 @@ function pubplot(options)
 %        options.LineWidth (1,1) {mustBeNumeric} = 2;
        options.legendOn (1,1) {mustBeNumericOrLogical} = false;
        options.PlotLineWidths (1,1) {mustBeNumeric} = 2;
-       options.plotsize (2, 1) {mustBeNumeric} = [nan, nan];
+       options.PlotSize (2, 1) {mustBeNumeric} = [nan, nan];
        options.xticks (1,1) {mustBeNumericOrLogical} = true;
        options.yticks (1,1) {mustBeNumericOrLogical} = true;
     end
-    if ~isnan(sum(options.plotsize))         
-        set(gcf, 'Position',  [100, 100, options.plotsize(1), options.plotsize(2)])
+    if ~isnan(sum(options.PlotSize))         
+        set(gcf, 'Position',  [100, 100, options.PlotSize(1), options.PlotSize(2)])
     end
     if options.legendOn
         set(legend,'fontsize', 25, 'fontweight', 'bold', 'Interpreter', 'latex');
