2c87194d29f63388f57d6943a4f507fb5c787eef
Giriraj Hiranandani <tghira16@gmail.com>
2022-06-21 16:19:27 +1000

    Update example.m

--- example.m
+++ example.m
@@ -1,10 +1,19 @@
 clearvars; close all;
 
-t = 0:.1:10;
-y = abs(sin(t)).^2;
+t = -5:.05:5;
+y = exp(-t.^2).*(4*t.^2 - 2);
+x = exp(-t.^2).*(8*t.^3 - 12*t);
+
+% Setting the width and the height of the plot
+width = 800;
+height = 400;
 
 hold on 
-pubplot("legendOn",true)
-plot(t, y)
-plot(t, 2*y)
+% Place this after "hold on" but before the rest of the plotting commands
+pubplot("legendOn", true, "PlotSize",[width height]) 
+
+xlabel('$x$ axis')
+ylabel('$y$ axis')
+plot(t, y, 'DisplayName','$e^{-x^2}H_2(x)$')
+plot(t, x, 'DisplayName','$e^{-x^2}H_3(x)$')
 hold off
